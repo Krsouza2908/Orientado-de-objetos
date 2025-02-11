d655ade2084355aa382d55aaa72c77b0fe0cc1cd
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-02-11 10:58:41 -0300

    Add files via upload

---
+++ aula_02.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = "kauã"
+p1.sobrenome = "Rosa"
+
+p1 = Pessoa()
+p1.nome = "taylor"
+p1.sobrenome = "Laigner"
+
+print(f"{p1.nome} {p1.sobrenome}")
