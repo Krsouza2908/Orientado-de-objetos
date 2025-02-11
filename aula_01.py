2d7be31a405d3ffe1e697d66dbe5545d9157322d
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-02-11 10:43:25 -0300

    Add files via upload

---
+++ aula_01.py
@@ -0,0 +1,12 @@
+'''
+class - Classes são moldes para criar novos objetos.
+As classes geram novos objetos (instâncias) que podem ter seus próprios atributos e métodos.
+Os objetos gerados pela classe podem usar seus dados internos para realizar várias ações.
+Por convenção, usamos PascalCase para nomes de classes.
+O que é PascalCase? Toda palavra inicia com letra maíuscula.
+'''
+nome = "kauã"
+print(nome.upper())
+print(isinstance(nome, str))
+print(isinstance(nome, int))
+print(isinstance(nome, float))
