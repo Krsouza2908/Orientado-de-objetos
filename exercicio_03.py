c44c8c7b65af142ec27812c72c54e92c70a3a8a1
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-03-17 09:07:40 -0300

    Add files via upload

---
+++ exercicio_03.py
@@ -0,0 +1,23 @@
+'''Exercício 03- Sistema de biblioteca com atributos privadas
+(-__livros_disponiveis e __livros_emprestados), e os seguintes métodos:
+adicionarLviros, emprestarLivro, devolverLivro e listarLivro().'''
+
+class Biblioteca:
+    def __init__(self):
+        #Lista privada para armazenar os livros disponíveis
+        self.__livros_disponiveis = []
+        #Lista para armazenar os livros emprestados e os usários responsáveis
+        self.__livros_emprestados = {}
+
+        def adicionarLivro(self):
+            pass
+
+        def emprestarLivro(self):
+            pass
+
+        def devolverLivro(self):
+            pass
+
+        def listarLivro(self):
+            pass
+
