a352a693e0068ad981804f4dfa8a67eac9be2d93
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-06-03 08:18:31 -0300

    Add files via upload

---
+++ Aula_01_2°Trimestre.py
@@ -0,0 +1,18 @@
+class Pessoa(self):
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar_nome(self):
+        print(self.nome, self.sobrenome, self.__class__.__nome__)    
+
+class Cliente(Pessoa):
+    pass
+
+class Aluno(Pessoa):
+    pass
+
+c1 = Cliente("Kauã", "Rosa")
+c1.falar_nome()
+a1 = Aluno("Vitor", "Alochio")
+a1.falar_nome()
---
+++ Aula_02_2°Trimestre.py
@@ -0,0 +1,34 @@
+#Herança simples
+class Jogador: #Super classe/Classe mãe
+    def __init__(self, altura, velocidade, passe, dible, precicao):
+        self.altura = altura
+        self.velocidade = velocidade
+        self.passe = passe
+        self.dible = dible
+        self.precicao = precicao
+
+    def passar(self):
+        print("Mirar")
+        print("Encostar na bola com a força de passe do jogador. ")
+
+    def chutar(self):
+        print("Mirar")
+        print("Encostar na bola com 2x a força de passe do jogador. ")
+
+class Jogador_Goleiro(Jogador):
+    def agarrar(self):
+        print("Pular")
+        print("Se esticar para pegar a bola")
+           
+class Jogador_Linha(Jogador_Goleiro):
+    pass
+
+jogador1 = Jogador_Goleiro(180, 60, 80, 20, 60)
+jogador2 = Jogador_Linha(174, 90, 80, 85, 80)
+
+jogador1.passar()
+jogador2.passar()
+jogador1.agarrar()
+jogador2.agarrar()
+
+print(Jogador_Linha.mro())
