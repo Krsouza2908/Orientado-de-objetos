52086e9d597ae1bbc7c026378d5c21a49ecf9f0c
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-08-04 11:50:19 -0300

    Add files via upload

--- Trabalho_Dupla.py
+++ Trabalho_Dupla.py
@@ -8,7 +8,7 @@ class Pessoa:
         self.endereco = endereco
     
     def mostrar_dados(self):
-        print(f"Nome: {self.nome}")
+        print(f"\nNome: {self.nome}")
         print(f"Idade: {self.idade}")
         print(f"CPF: {self.cpf}")
         print(f"RG: {self.rg}")
@@ -25,11 +25,10 @@ class Aluno(Pessoa):
         super().mostrar_dados()
         print(f"RA: {self.ra}")
         print(f"Curso: {self.curso}")
-        
 
 class Professor(Pessoa):
-    def __init__(self, nome, idade, cpf, nf, disciplina):
-        super().__init__(nome, idade, cpf,)
+    def __init__(self, nome, idade, cpf, rg, telefone, endereco, nf, disciplina):
+        super().__init__(nome, idade, cpf, rg, telefone, endereco)
         self.nf = nf
         self.disciplina = disciplina
 
@@ -39,24 +38,60 @@ class Professor(Pessoa):
         print(f"Disciplina: {self.disciplina}")
 
 class Funcionario(Pessoa):
-    def __init__(self, nome, idade, cpf, setor, funcao):
-        super().__init__(nome, idade, cpf,)
+    def __init__(self, nome, idade, cpf, rg, telefone, endereco, setor, funcao):
+        super().__init__(nome, idade, cpf, rg, telefone, endereco)
         self.setor = setor
         self.funcao = funcao
 
     def mostrar_dados(self):
         super().mostrar_dados()
         print(f"Setor: {self.setor}")
-        print(f"Função: {self.funcao}")       
+        print(f"Função: {self.funcao}")
 
-nomeA = input("Diite seu nome: ")
-idadeA = int(input("Diite sua Idade: "))
-cpfA = input("Diite o seu CPF: ")
-rgA = int(input("Diite seu RG: "))
-telefoneA = input("Diite seu telefone: ")
-enderecoA = input("Diite seu endereço: ")
 
-pessoa1 = Pessoa(nomeA, idadeA, cpfA, telefoneA, enderecoA )
+print("------------Cadastro de Aluno------------")
+nomeA = input("Digite seu nome: ")
+idadeA = int(input("Digite sua idade: "))
+cpfA = input("Digite o seu CPF: ")
+rgA = input("Digite seu RG: ")
+telefoneA = input("Digite seu telefone: ")
+enderecoA = input("Digite seu endereço: ")
+raA = input("Digite seu RA: ")
+cursoA = input("Digite seu curso: ")
+pessoa1 = Aluno(nomeA, idadeA, cpfA, rgA, telefoneA, enderecoA, raA, cursoA)
 
-print("\nDados Cadastrados")
+
+print("\n------------Cadastro de Professor------------")
+nomeP = input("Digite seu nome: ")
+idadeP = int(input("Digite sua idade: "))
+cpfP = input("Digite o seu CPF: ")
+rgP = input("Digite seu RG: ")
+telefoneP = input("Digite seu telefone: ")
+enderecoP = input("Digite seu endereço: ")
+nfP = int(input("Digite seu número funcional: "))
+disciplinaP = input("Digite sua disciplina: ")
+pessoa2 = Professor(nomeP, idadeP, cpfP, rgP, telefoneP, enderecoP, nfP, disciplinaP)
+
+
+print("\n------------Cadastro de Funcionário------------")
+nomeF = input("Digite seu nome: ")
+idadeF = int(input("Digite sua idade: "))
+cpfF = input("Digite o seu CPF: ")
+rgF = input("Digite seu RG: ")
+telefoneF = input("Digite seu telefone: ")
+enderecoF = input("Digite seu endereço: ")
+setorF = input("Digite seu setor: ")
+funcaoF = input("Digite sua função: ")
+pessoa3 = Funcionario(nomeF, idadeF, cpfF, rgF, telefoneF, enderecoF, setorF, funcaoF)
+
+
+print("\n------------Dados Cadastrados------------")
+
+print("\n===== DADOS DO ALUNO =====")
 pessoa1.mostrar_dados()
+
+print("\n===== DADOS DO PROFESSOR =====")
+pessoa2.mostrar_dados()
+
+print("\n===== DADOS DO FUNCIONÁRIO =====")
+pessoa3.mostrar_dados()
