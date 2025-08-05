1a1ad95827d68b7703afe3c43cd3caf398a37cdc
Krsouza2908 <114338532+Krsouza2908@users.noreply.github.com>
2025-08-05 07:46:42 -0300

    Add files via upload

--- Tabalho_nenel.py
+++ Tabalho_nenel.py
@@ -8,90 +8,93 @@ class Pessoa:
         self.endereco = endereco
     
     def mostrar_dados(self):
-        print(f"\nNome: {self.nome}")
-        print(f"Idade: {self.idade}")
-        print(f"CPF: {self.cpf}")
-        print(f"RG: {self.rg}")
-        print(f"Telefone: {self.telefone}")
-        print(f"Endereço: {self.endereco}")
+        print(f"\nNome cadastrado: {self.nome}")
+        print(f"Idade cadastrada: {self.idade}")
+        print(f"CPF cadastrado: {self.cpf}")
+        print(f"RG cadastrado: {self.rg}")
+        print(f"Telefone cadastrado: {self.telefone}")
+        print(f"Endereço cadastrado: {self.endereco}")
 
 class Aluno(Pessoa):
-    def __init__(self, nome, idade, cpf, rg, telefone, endereco, ra, curso):
+    def __init__(self, nome, idade, cpf, rg, telefone, endereco, curso, ra):
         super().__init__(nome, idade, cpf, rg, telefone, endereco)
         self.ra = ra
         self.curso = curso
 
     def mostrar_dados(self):
         super().mostrar_dados()
-        print(f"RA: {self.ra}")
-        print(f"Curso: {self.curso}")
+        print(f"Curso cadastrado: {self.curso}")
+        print(f"RA cadastrado: {self.ra}")
 
 class Professor(Pessoa):
-    def __init__(self, nome, idade, cpf, rg, telefone, endereco, numerofuncional, disciplina):
+    def __init__(self, nome, idade, cpf, rg, telefone, endereco, disciplina, numero_funcional):
         super().__init__(nome, idade, cpf, rg, telefone, endereco)
-        self.nf = nf
+        self.numero_funcional = numero_funcional
         self.disciplina = disciplina
 
     def mostrar_dados(self):
         super().mostrar_dados()
-        print(f"Número Funcional: {self.nf}")
-        print(f"Disciplina: {self.disciplina}")
+        print(f"Disciplina cadastrada: {self.disciplina}")
+        print(f"Número funcional cadastrado: {self.numero_funcional}")
 
 class Funcionario(Pessoa):
-    def __init__(self, nome, idade, cpf, rg, telefone, endereco, setor, funcao):
+    def __init__(self, nome, idade, cpf, rg, telefone, endereco, funcao, setor):
         super().__init__(nome, idade, cpf, rg, telefone, endereco)
         self.setor = setor
         self.funcao = funcao
 
     def mostrar_dados(self):
         super().mostrar_dados()
-        print(f"Setor: {self.setor}")
-        print(f"Função: {self.funcao}")
+        print(f"Função cadastrada: {self.funcao}")
+        print(f"Setor cadastrado: {self.setor}")
 
-
-print("------------Cadastro de Aluno------------")
+# Cadastro do aluno
+print("------------ Cadastro do Aluno ------------")
 nomeA = input("Informe seu nome: ")
 idadeA = int(input("Informe sua idade: "))
-cpfA = input("Informe o seu CPF: ")
+cpfA = input("Informe seu CPF: ")
 rgA = input("Informe seu RG: ")
 telefoneA = input("Informe seu telefone: ")
-enderecoA = input("Digite seu endereço: ")
-raA = input("Informe seu RA: ")
+enderecoA = input("Informe seu endereço: ")
 cursoA = input("Informe seu curso: ")
-pessoa1 = Aluno(nomeA, idadeA, cpfA, rgA, telefoneA, enderecoA, raA, cursoA)
+raA = input("Informe seu RA: ")
+
+aluno = Aluno(nomeA, idadeA, cpfA, rgA, telefoneA, enderecoA, cursoA, raA)
+
+# Cadastro do professor
+print("\n------------ Cadastro do Professor ------------")
+nomeP = input("Informe seu nome: ")
+idadeP = int(input("Informe sua idade: "))
+cpfP = input("Informe seu CPF: ")
+rgP = input("Informe seu RG: ")
+telefoneP = input("Informe seu telefone: ")
+enderecoP = input("Informe seu endereço: ")
+disciplinaP = input("Informe sua disciplina: ")
+numeroFuncionalP = input("Informe seu número funcional: ")
+
+professor = Professor(nomeP, idadeP, cpfP, rgP, telefoneP, enderecoP, disciplinaP, numeroFuncionalP)
+
+# Cadastro do funcionário
+print("\n------------ Cadastro do Funcionário ------------")
+nomeF = input("Informe seu nome: ")
+idadeF = int(input("Informe sua idade: "))
+cpfF = input("Informe seu CPF: ")
+rgF = input("Informe seu RG: ")
+telefoneF = input("Informe seu telefone: ")
+enderecoF = input("Informe seu endereço: ")
+funcaoF = input("Informe sua função: ")
+setorF = input("Informe seu setor: ")
+
+funcionario = Funcionario(nomeF, idadeF, cpfF, rgF, telefoneF, enderecoF, funcaoF, setorF)
 
 
-print("\n----------Cadastro de Professor----------")
-nomeProfessor = input("Informe seu nome: ")
-idadeProfessor = int(input("Informe sua idade: "))
-cpfProfessor = input("Informe o seu CPF: ")
-rgProfessor = input("Informe seu RG: ")
-telefoneProfessor = input("Informe seu telefone: ")
-enderecoProfessor = input("Informe seu endereço: ")
-numerofuncionalProfessor = int(input("Informe seu número funcional: "))
-disciplinaProfessor = input("Informe sua disciplina: ")
-    pessoa2 = Professor(nomeProfessor, idadeProfessor, cpfProfessor, numerofuncionalProfessor, telefoneProfessor, enderecoProfessor, numerofuncionalProfessor, disciplinaProfessor)
+print("\n------------ DADOS CADASTRADOS ------------")
 
+print("\n--- Dados do Aluno ---")
+aluno.mostrar_dados()
 
-print("\n------------Cadastro de Funcionário------------")
-nomeFuncionario = input("Informe seu nome: ")
-idadeFuncionario = int(input("Informe sua idade: "))
-cpfFuncionario = input("Informe o seu CPF: ")
-rgFuncionario = input("Informe seu RG: ")
-telefoneFuncionario = input("Informe seu telefone: ")
-enderecoFuncionario = input("Informe seu endereço: ")
-setorFuncionario = input("Informe seu setor: ")
-funcaoFuncionario = input("Informe sua função: ")
-pessoa3 = Funcionario(nomeFuncionario, idadeFuncionario, cpfFuncionario, rgFuncionario, telefoneFuncionario, enderecoFuncionario, setorFuncionario, funcaoFuncionario)
+print("\n--- Dados do Professor ---")
+professor.mostrar_dados()
 
-
-print("\n----------Dados Cadastrados----------")
-
-print("\n------Dados cadastrados do Aluno -----")
-pessoa1.mostrar_dados()
-
-print("\n----- Dados cadastrados do Professor -----")
-pessoa2.mostrar_dados()
-
-print("\n----- Dados cadastrados do  Funcionário -----")
-pessoa3.mostrar_dados()
+print("\n--- Dados do Funcionário ---")
+funcionario.mostrar_dados()
